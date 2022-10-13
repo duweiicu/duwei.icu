e21053f841639f53d9f4ac09772a61ea26dfc5f4
duweiicu <duweiicu@proton.me>
2022-10-13 23:46:30 +0800

    [update]

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const LANGUAGE = document.getElementsByTagName("html")[0].getAttribute("lang"),
     URL = window.location.href;
 
+localStorage.setItem("language", LANGUAGE);
+
 function changeLanguage() {
     if (LANGUAGE === "en") {
         window.location.href = URL.replace("/en/", "/zh/");
